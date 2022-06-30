f51ac7cb700e574217a79b448159d4623748bcaf
HSW425448692 <73380363+HSW42544869@users.noreply.github.com>
2022-07-01 00:00:49 +0800

    0630
    CS

---
+++ Assets/1 Scripts/LearnAPIStatic.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+/// <summary>
+/// ¾Ç²ß ÀRºA API
+/// ÀRºA static
+/// </summary>
+public class LearnAPIStatic : MonoBehaviour
+{
+    // ÀRºAÄÝ©Ê static 
+}
--- Assets/1 Scripts/SystemControl.cs
+++ Assets/1 Scripts/SystemControl.cs
@@ -3,8 +3,45 @@ using UnityEngine;
 //  命名空間 namespace 空間名稱 { 該空間的內容 }
 namespace WEI
 {
+    /// <summary>
+    /// 玩家控制系統
+    /// 物件旋轉、發射彈珠
+    /// </summary>
     public class SystemControl : MonoBehaviour
     {
+        #region 資料
+        [Header("箭頭物件")]
+        public GameObject arrow;
+        [Header("旋轉速度"),Range(0,500)]
+        public float speedTurn = 10.5f;
+        [Header("彈珠物件")]
+        public GameObject marble;
+        [Header("彈珠可發射總數"),Range(0, 1000)]
+        public int canShootMarbleTotal = 15;
+        #endregion
 
+        #region 事件
+        #endregion
+
+        #region 方法
+        /// <summary>
+        /// 角色選轉
+        /// </summary>
+        private void TurnChsrscter()
+        { 
+        }
+        /// <summary>
+        /// 發射彈珠
+        /// </summary>
+        private void ShootMarbleTotal()
+        { 
+        }
+        /// <summary>
+        /// 回收彈珠
+        /// </summary>
+        private void RecycleMarble()
+        { 
+        }
+        #endregion
     }
 }
